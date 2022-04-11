b74c72a9a21c5ced15162659164261717e8800c5
MohitSaraff <saraffmohit@gmail.com>
2022-04-11 19:50:56 +0530

    Update pay.php

--- pay.php
+++ pay.php
@@ -78,7 +78,7 @@ $json = json_encode($data);
 ?>
 
 
-<form action="verify.html" method="POST">
+<form action="verify.php" method="POST">
   <script
     src="https://checkout.razorpay.com/v1/checkout.js"
     data-key="<?php echo $data['key']?>"
